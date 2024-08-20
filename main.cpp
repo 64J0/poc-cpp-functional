2b5eaf645365573ee60fa9659a7b5982bb2f1168
64J0 <vinigaio97@gmail.com>
2024-08-20 20:28:30 -0300

    change lambdas to get scope by reference and start using lambda functions with no arguments to avoid passing values to the higher-order function run_and_measure_time

--- main.cpp
+++ main.cpp
@@ -38,9 +38,9 @@ public:
   // Docs for lambda expressions in C++ and their capture clause
   // https://learn.microsoft.com/en-us/cpp/cpp/lambda-expressions-in-cpp?view=msvc-170#capture-clause
   static auto linspace_curied(const int &start) {
-    return [=](const int &end) {
-      return [=](const int &points) {
-        return [=](const bool &endpoint) {
+    return [&](const int &end) {
+      return [&](const int &points) {
+        return [&](const bool &endpoint) {
           return NumpyCpp::linspace(start, end, points, endpoint);
         };
       };
@@ -50,10 +50,13 @@ public:
 
 // ========================================
 // Begin helper functions
-template <typename T>
-std::vector<float> run_and_measure_time(const bool &endpoint, T func) {
+template <typename T> std::vector<float> run_and_measure_time(T func) {
+  // Debug to verify if the lambda is not being called in the incorrect
+  // order
+  // std::cout << "Outer body!\n";
+
   const auto t_begin = high_resolution_clock::now();
-  const auto exec_result = func(endpoint);
+  const auto exec_result = func();
   const auto t_end = high_resolution_clock::now();
 
   const nanoseconds t_diff = t_end - t_begin;
@@ -81,28 +84,36 @@ template <typename T> void print(const std::vector<T> &res) {
 int main() {
   const std::string line(60, '-');
 
-  const std::vector<int> start{0, 0, 0, 0, 0, 2, 2};
-  const std::vector<int> end{0, 6, 6, 4, 4, 3, 3};
-  const std::vector<int> points{5, 4, 4, 10, 10, 5, 5};
-  const std::vector<bool> endpoint{true, true, false, true, false, false, true};
+  const std::vector<int> start{0, 0, 0, 0, 0, 2, 2, 3};
+  const std::vector<int> end{0, 6, 6, 4, 4, 3, 3, 2};
+  const std::vector<int> points{5, 4, 4, 10, 10, 5, 5, 5};
+  const std::vector<bool> endpoint{true,  true,  false, true,
+                                   false, false, true,  true};
 
   const auto start_size = start.size();
   for (auto i = 0; i < start_size; ++i) {
     std::cout << line << "\n\n";
 
-    // auto res = NumpyCpp::linspace(start[i], end[i], points[i], endpoint[i]);
-    // Partial application with bind
-    std::cout << "Partial application with std::bind:\n";
-    auto p1 = std::bind(NumpyCpp::linspace, start[i], end[i], points[i],
-                        std::placeholders::_1);
-    auto res1 = run_and_measure_time(endpoint[i], p1);
+    auto res1 = run_and_measure_time([&]() {
+      // std::cout << "Inner body!\n";
+      return NumpyCpp::linspace(start[i], end[i], points[i], endpoint[i]);
+    });
     print(res1);
 
     // Partial application with curied lambda functions
-    std::cout << "\nPartial application with curied lambda functions:\n";
-    auto p2 = NumpyCpp::linspace_curied(start[i])(end[i])(points[i]);
-    auto res2 = run_and_measure_time(endpoint[i], p2);
+    std::cout << "\nPartial application with curied lambda functions\n";
+    auto res2 = run_and_measure_time([&]() {
+      return NumpyCpp::linspace_curied(start[i])(end[i])(points[i])(
+          endpoint[i]);
+    });
     print(res2);
+
+    // Partial application with std::bind
+    std::cout << "\nPartial application with std::bind\n";
+    auto p3 = std::bind(NumpyCpp::linspace, start[i], end[i], points[i],
+                        std::placeholders::_1);
+    auto res3 = run_and_measure_time([&]() { return p3(endpoint[i]); });
+    print(res3);
   }
 
   return 0;
